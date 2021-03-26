b486f81aeba9dbac2510395fd2479c5a023d4a99
patrycja-io <patrycjatyra@protonmail.com>
2021-03-26 14:13:34 +0000

    setState & Class

--- simple-counter-react-state/src/Counter.js
+++ simple-counter-react-state/src/Counter.js
@@ -4,8 +4,25 @@ class Counter extends Component {
 constructor(props) {
     super(props);
     this.state ={
-      count:3,
+      count:0,
     };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+
+}
+
+increment() {
+  this.setState({count:this.state.count + 1 });
+}
+
+decrement() {
+  this.setState({count:this.state.count - 1 });
+}
+
+reset() {
+  this.setState({count:0});
 }
   render() {
     const { count } = this.state;
@@ -14,9 +31,9 @@ constructor(props) {
       <div className="Counter">
         <p className="count">{count}</p>
         <section className="controls">
-          <button >Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
