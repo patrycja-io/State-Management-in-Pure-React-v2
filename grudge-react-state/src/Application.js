dc6b7ce97a2bc88b39bdbce312158d4e62801d71
patrycja-io <patrycjatyra@protonmail.com>
2021-03-30 17:54:27 +0100

    creating context provider

--- grudge-react-state/src/Application.js
+++ grudge-react-state/src/Application.js
@@ -1,26 +1,7 @@
 import React, { useReducer, useCallback } from 'react';
 
-import id from 'uuid/v4';
 import Grudges from './Grudges';
 import NewGrudge from './NewGrudge';
-
-import initialState from './initialState';
-
-const GRUDGE_ADD = 'GRUDGE_ADD';
-const GRUDGE_FORGIVE = "GRUDGE_FORGIVE";
-
-const reducer = (state, action) => {
- if (action.type === GRUDGE_ADD) {
-   return [action.payload, ...state ];
- }
- if (action.type === GRUDGE_FORGIVE) {
-  return state.map(grudge => {
-     if (grudge.id !== action.payload.id) return grudge;
-     return {...grudge, forgiven:!grudge.forgiven};
-   })
- }
- return state;
-};
 
 const Application = () => {
   const [grudges, dispatch ]= useReducer(reducer, initialState);
